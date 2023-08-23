16087ffe38c2906b5f3ab652a8c472e07cde11d5
GregAndrik <andrikgreg@gmail.com>
2023-08-23 23:23:26 +0300

    changed Argus logo

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,7 +3,7 @@ import './footer.css'
 const Footer = () => {
   return (
     <footer>
-      <div className='footer_copyright'>
+      <div className='footer-copyright'>
         <p>&copy; {new Date().getFullYear()} _cryptonym. All rights reserved.</p>
       </div>
     </footer>
