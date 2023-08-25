1fb7008866bc3d48001fb5eb4cfa86f1599922aa
GregAndrik <andrikgreg@gmail.com>
2023-08-25 17:17:23 +0300

    added mobile title

--- src/components/views/home/Home.js
+++ src/components/views/home/Home.js
@@ -7,6 +7,9 @@ const Home = () => {
     <>
     <div className='home-container'>
       <div className='home-background'>
+        <div>
+          <h1 className='home-mobile-title'>_cryptonym</h1>
+        </div>
         <img className='home-image' src={argus} alt='Argus logo'/>
       </div>
     </div>
